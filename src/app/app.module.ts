e7229d4fc8d6049746aa29ea227fed92e3a86ee7
Sergey Burov <sergey.burov.ext@siemens.com>
2021-04-23 15:04:43 +0300

    Implemented bug reproduction.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {IgxTabsModule} from 'igniteui-angular';
+import { IgxDataChartCoreModule, IgxNumericXAxisModule, IgxNumericYAxisModule } from 'igniteui-angular-charts';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    IgxTabsModule,
+    IgxDataChartCoreModule,
+    IgxNumericXAxisModule,
+    IgxNumericYAxisModule
   ],
   providers: [],
   bootstrap: [AppComponent]
